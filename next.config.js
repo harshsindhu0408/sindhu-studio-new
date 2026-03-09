8f3a0a6162b59e632f019c694618181e3706f1e0
harshsindhu0408 <harshsindhu0402@gmail.com>
2026-03-09 23:32:22 +0530

    added cache support for images in nextjs config file

--- next.config.js
+++ next.config.js
@@ -1,30 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.unsplash.com",
-                pathname: "**",
-            },
-            {
-                protocol: "https",
-                hostname: "plus.unsplash.com",
-                pathname: "**",
-            },
-        ],
-    },
-    // Performance optimizations
-    experimental: {
-        // Optimize CSS
-        optimizeCss: true,
-    },
-    // Enable compression
-    compress: true,
-    // Reduce powered by header
-    poweredByHeader: false,
+  images: {
+    minimumCacheTTL: 1728000,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        pathname: "**",
+      },
+    ],
+  },
+  // Performance optimizations
+  experimental: {
+    // Optimize CSS
+    optimizeCss: true,
+  },
+  // Enable compression
+  compress: true,
+  // Reduce powered by header
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
-
-
